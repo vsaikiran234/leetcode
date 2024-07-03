bfb03a6bcf58a1dec11a126ea529b36503c749c5
vsaikiran6 <112771970+vsaikiran234@users.noreply.github.com>
2024-07-03 22:15:05 +0530

    Time: 72 ms (23.61%), Space: 49.9 MB (5.17%) - LeetHub

---
+++ 2821-timeout-cancellation/2821-timeout-cancellation.js
@@ -0,0 +1,10 @@
+var cancellable = function(fn, args, t) {
+    let timeout = setTimeout(() => {
+        fn(...args)
+    }, t)
+
+    return function() {
+        clearTimeout(timeout)
+    }
+
+};
