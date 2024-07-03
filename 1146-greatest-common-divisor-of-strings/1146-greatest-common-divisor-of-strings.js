a78225e143a6872c6de6f78ee9f773f58f9be8e7
vsaikiran6 <112771970+vsaikiran234@users.noreply.github.com>
2024-07-03 21:34:16 +0530

    Time: 53 ms (61.84%), Space: 49.1 MB (67.71%) - LeetHub

---
+++ 1146-greatest-common-divisor-of-strings/1146-greatest-common-divisor-of-strings.js
@@ -0,0 +1,16 @@
+var gcdOfStrings = function(str1, str2) {
+    if ((str1 + str2) !== (str2 + str1)) {
+        return ""
+    }
+
+    let len1 = str1.length
+    let len2 = str2.length
+
+    while (len2 != 0) {
+        let temp = len2
+        len2 = len1 % len2
+        len1 = temp
+    }
+
+    return str1.substring(0, len1)
+};
