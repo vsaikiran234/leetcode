92c8861187cd5f8ed7c60521e603b0efb38dda56
vsaikiran6 <112771970+vsaikiran234@users.noreply.github.com>
2024-07-07 22:27:21 +0530

    Time: 528 ms (5.03%), Space: 172.7 MB (65.39%) - LeetHub

--- 0146-lru-cache/0146-lru-cache.cpp
+++ 0146-lru-cache/0146-lru-cache.cpp
@@ -1,62 +1,65 @@
-class LRUCache {
-public:
-    LRUCache(int capacity) {
-        this->capacity = capacity;
-    }
-                                            
-    int get(int key) {
-        if(!g.count(key) || g[key] == -1) return -1;
-        p.push_back(key);
-        visited[key]++;
-        return g[key];
-    }
-
-    void put(int key, int value) {
-        if(!g.count(key)|| g[key] == -1) count++;
-        else visited[key]++;
-        if(count > capacity){
-            while(visited[p.front()]) visited[p.front()]--, p.pop_front();
-            g[p.front()] = -1;
-            p.pop_front();
-            count--;
-        }
-        p.push_back(key);
-        g[key] = value;
-    }
-
-private:
-    unordered_map<int, int>g;
-    unordered_map<int, int>visited;
-    deque<int>p;
-    int count = 0;
-    int capacity = 0;
+struct Node { // struct Node{}
+  int key; // int key;
+  int value; // int value;
+  shared_ptr<Node> prev; // shared_ptr<Node> prev;
+  shared_ptr<Node> next;  // shared_ptr<Node> next;
+  Node(int key, int value) : key(key), value(value) {} // Node(int key, int value)
 };
 
+class LRUCache { // class LRUCache
+ public: // public:
+  LRUCache(int capacity) : capacity(capacity) {
+    join(head, tail); // join(head, tail)
+  }
 
+  int get(int key) { // int  get(int key)
+    const auto it = keyToNode.find(key); // const aut it = keyToNode.find(key);
+    if (it == keyToNode.cend()) // if (it == KeyToNode.cend())
+      return -1; // return -1
 
-                                                                       
-                                                                      
-                                                                            
+    shared_ptr<Node> node = it->second; // shared_ptr<Node> node = it->second
+    remove(node); // remove(node);
+    moveToHead(node); // moveToHead(node);
+    return node->value; // return node->value;
+  }
 
-                                                                                                                                                                                                                                                                  
-                                                                                                                                                                                                                                                                       
+  void put(int key, int value) { // void put(intkey, int value)
+    if (const auto it = keyToNode.find(key); it != keyToNode.cend()) {
+      shared_ptr<Node> node = it->second; // shared_ptr<Node> node = it->secong;
+      node->value = value; // node->value = value;
+      remove(node); // remove(node);
 
+      moveToHead(node); // moveToHead(node);
+      return; // return ;
+    }
 
-   
+    if (keyToNode.size() == capacity) { // if (keyToNode.size() == capacity)
+      shared_ptr<Node> lastNode = tail->prev; // shared_ptr<Node> lastNode = tail->prev;
+      keyToNode.erase(lastNode->key); // KeyToNode.erase(lastNode->key);
+      remove(lastNode); // remove(lastNode);
+    }
 
+    moveToHead(make_shared<Node>(key, value)); // moveToHead(make_shared<node>(key, value));
+    keyToNode[key] = head->next; // keyToNode[key] = head->next;
+  }
 
-    
+ private: // private: 
+  const int capacity; // const int capacity;
+  unordered_map<int, shared_ptr<Node>> keyToNode; // unordered_map<int, shared_ptr<Node>> keyToNode;
+  shared_ptr<Node> head = make_shared<Node>(-1, -1);// shared_ptr<Node> head = make_shared<Node>(-1, -1)
+  shared_ptr<Node> tail = make_shared<Node>(-1, -1); // shared_ptr<Node> tail = make_shared<Node>(-1, -1)
 
+  void join(shared_ptr<Node> node1, shared_ptr<Node> node2) { // void join(shared_ptr<node> node1, shared_ptr<Node> node2)
+    node1->next = node2; // node->next = node2;
+    node2->prev = node1; // node2->prev = node1;
+  }
 
+  void moveToHead(shared_ptr<Node> node) { // void moveToHead(shared_ptr<node> node)
+    join(node, head->next); // join(node, head->next);
+    join(head, node); // join(head, node);
+  }
 
-    
-  
-        
-
-
-
-
-
-
-
-
+  void remove(shared_ptr<Node> node) { // void remove(shared_ptr<Node> node)
+    join(node->prev, node->next); // join(node->prev, node->next);
+  }
+};
