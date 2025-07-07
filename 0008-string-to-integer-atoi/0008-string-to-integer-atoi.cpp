d70ed5cd7358e46ef7c02b6a478346db465ce8f8
vsaikiran6 <112771970+vsaikiran234@users.noreply.github.com>
2025-07-07 18:57:05 +0530

    Time: 0 ms (100%), Space: 9.3 MB (14.05%) - LeetHub

---
+++ 0008-string-to-integer-atoi/0008-string-to-integer-atoi.cpp
@@ -0,0 +1,34 @@
+
+
+class Solution { // class Solution 
+ public: // punblic:
+  int myAtoi(string s) { // int myAtoi(string s) {}
+    trim(s);       // trim(s);
+    if (s.empty())  // if (s.empty())
+      return 0;  // return 0;
+
+    const int sign = s[0] == '-' ? -1 : 1; // const int sign = s[0] == '-' ? -1 : 1;
+    if (s[0] == '+' || s[0] == '-')
+      s = s.substr(1);
+
+    long num = 0;
+
+    for (const char c : s) {
+      if (!isdigit(c))
+        break;
+      num = num * 10 + (c - '0'); // num = num * 10 + (c - '0');
+      if (sign * num < INT_MIN)
+        return INT_MIN;
+      if (sign * num > INT_MAX)
+        return INT_MAX;
+    }
+
+    return sign * num; // return sign * num;
+  }
+
+ private:
+  void trim(string& s) {    
+    s.erase(0, s.find_first_not_of(' '));
+    s.erase(s.find_last_not_of(' ') + 1);
+  }
+};
